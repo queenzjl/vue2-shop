d96620551e9771ff98c3bac55d61ebd1b86d09fc
queenzjl <15733267376@163.com>
2018-04-29 21:01:34 +0800

    vue-lazyload图片懒加载

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import VueLazyLoad from 'vue-lazyload'
+Vue.use(VueLazyLoad, {
+    loading: 'static/loading-svg/loading-bars.svg',
+    try: 3
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
