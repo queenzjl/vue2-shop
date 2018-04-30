1873bc1ac9ad61630e43d3cfc2a2f6ada80ec67c
queenzjl <15733267376@163.com>
2018-04-30 17:29:51 +0800

    购物车需求的初始化(代码拆分)

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodList from '@/view/GoodList'
-
+import Cart from '@/view/Cart'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'GoodList',
-        component: GoodList
-
-    }]
+            path: '/',
+            name: 'GoodList',
+            component: GoodList
+        },
+        {
+            path: '/goodList',
+            name: 'GoodList',
+            component: GoodList
+        },
+        {
+            path: '/cart',
+            name: 'Cart',
+            component: Cart
+        }
+    ]
 })
